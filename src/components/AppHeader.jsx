43f4d493a2532f8a176b24702aaeb88f4ed42875
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-07-07 13:05:43 +0200

    feat: AppHeader

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -13,31 +13,41 @@ const AppHeader = () => {
             url: '/books'
         },
 
-    ]
+    ];
 
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+            <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
                 <div className="container-fluid">
-                    <NavLink className="navbar-brand">
+                    <NavLink className="navbar-brand" to="/">
                         My Books
                     </NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span className="navbar-toggler-icon"></span>
                     </button>
+
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
-                            {navList.map((curLink, index) => {
+                            {navList.map((curLink, index) => (
                                 <li className="nav-item" key={index}>
                                     <NavLink
-                                        className="nav-link active"
-                                        aria-current="page"
+                                        className = 'nav-link'
+                                        
                                         to={curLink.url}>
                                         {curLink.title}
                                     </NavLink>
                                 </li>
-                            })}
+                            ))}
                         </ul>
                     </div>
                 </div>
