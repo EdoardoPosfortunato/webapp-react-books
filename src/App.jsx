c43a96a5d9c71efc46594e45424b101ec9b52558
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-07-07 12:38:47 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import GuestLayout from './layout/guestLayout'
+import Home from './pages/Home'
+import Books from './pages/Books'
+
+
+function App() {
+
+  // QUESTA chiamata di PRova per vedere se fa il collegamento
+  // useEffect(() => {
+  //   axios.get("http://localhost:3000/books").then((resp) => {
+  //     console.log(resp);
+  //   })
+  // }, [])
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<GuestLayout />}>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/books' element={<Books />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Books.jsx
@@ -0,0 +1,9 @@
+const Books = () => {
+    return (
+        <>
+        <h1>Ciao sono la Books</h1>
+        </>
+    )
+}
+
+export default Books;
