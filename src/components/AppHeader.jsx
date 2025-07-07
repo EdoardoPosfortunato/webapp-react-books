656f8136c670ca7a2a73047d0df7878e68baed50
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-07-07 12:54:39 +0200

    feat: headerNav

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,49 @@
+import { NavLink, Outlet } from "react-router-dom"
+
+
+const AppHeader = () => {
+
+    const navList = [
+        {
+            title: 'Home',
+            url: '/'
+        },
+        {
+            title: 'Libri',
+            url: '/books'
+        },
+
+    ]
+
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand">
+                        My Books
+                    </NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            {navList.map((curLink, index) => {
+                                <li className="nav-item" key={index}>
+                                    <NavLink
+                                        className="nav-link active"
+                                        aria-current="page"
+                                        to={curLink.url}>
+                                        {curLink.title}
+                                    </NavLink>
+                                </li>
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default AppHeader;
--- src/layout/GuestLayout.jsx
+++ src/layout/GuestLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
+import AppHeader from "../components/AppHeader";
 
 
 const GuestLayout = () => {
     return (
         <>
+        <AppHeader />
         <Outlet />
         </>
     )
